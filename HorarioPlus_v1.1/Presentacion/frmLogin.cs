429a02f9c7cd9190d3ed567388d598502b43ce72
Rick <77352236+richcrd@users.noreply.github.com>
2024-03-06 09:41:36 -0600

    Initial Commit
    Versión estándar para encajonar elementos adicionales en HorarioPlus

---
+++ HorarioPlus_v1.1/Presentacion/frmLogin.cs
@@ -0,0 +1,95 @@
+﻿using HorarioPlus_v1._1.Datos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Drawing;
+
+namespace HorarioPlus_v1._1.Presentacion
+{
+    public partial class frmLogin : Form
+    {
+        private const string RUTA_ARCHIVO_EMPLEADOS = @"E:\Horario+\archivos_empleados\Empleados.json";
+        public frmLogin()
+        {
+            InitializeComponent();
+            timerHora.Start();
+        }
+
+        private void timerHora_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+
+        public Empleados BuscarEmpleado(string idEmpleado)
+        {
+            try
+            {
+                // Leer el contenido del archivo JSON
+                string json = File.ReadAllText(RUTA_ARCHIVO_EMPLEADOS);
+
+                // Deserializar el contenido del archivo JSON en una lista de objetos Empleado
+                List<Empleados> empleados = JsonSerializer.Deserialize<List<Empleados>>(json);
+
+                // Buscar el empleado correspondiente al ID
+                return empleados.Find(e => e.IdEmpleado == idEmpleado);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al leer el archivo de empleados: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Devolver un nuevo objeto Empleado en caso de error
+                return new Empleados();
+            }
+        }
+
+        private void btnMarcarRegistro_Click(object sender, EventArgs e)
+        {
+            bool volverAMarcar = true;
+
+            while (volverAMarcar)
+            {
+                string idEmpleado = txtEmpleadoId.Text;
+                Empleados empleado = BuscarEmpleado(idEmpleado);
+
+                if (empleado != null)
+                {
+                    string mensaje = $"{empleado.Nombre} {empleado.Apellido1} registrado a las {DateTime.Now.ToString("hh:mm:ss tt")}";
+                    MessageBox.Show(mensaje, "Registro Exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtEmpleadoId.Clear();
+
+                    if (empleado.Rol == "Administrador")
+                    {
+                        DialogResult resultado = MessageBox.Show("¿Desea ingresar al sistema como administrador?", "Confirmar Acceso", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                        if (resultado == DialogResult.Yes)
+                        {
+                            this.Hide();
+                            frmPanelAdministrador frmPanelAdmin = new frmPanelAdministrador(empleado.Nombre, empleado.Apellido1);
+                            frmPanelAdmin.ShowDialog();
+                        }
+                        else
+                        {
+                            // Si el usuario elige "No", volvemos a marcar
+                            volverAMarcar = true;
+                        }
+                    }
+                }
+                else
+                {
+                    // Si el ID del empleado no existe, mostramos un mensaje de error
+                    MessageBox.Show("El ID del empleado no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    volverAMarcar = false; // Salimos del bucle
+                }
+            }
+        }
+
+
+        private void lblCerrarApp_Click(object sender, EventArgs e)
+        {
+            frmCerrarSistema frmCerrarConfirmacionDialogo = new frmCerrarSistema();
+            frmCerrarConfirmacionDialogo.ShowDialog();
+        }
+    }
+}
---
+++ HorarioPlus_v1.1/Presentacion/frmPanelAdministrador.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace HorarioPlus_v1._1.Presentacion
+{
+    partial class frmPanelAdministrador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuPanel = new System.Windows.Forms.MenuStrip();
+            this.asistenciaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEmpleados = new FontAwesome.Sharp.IconMenuItem();
+            this.submenuNuevoRegistro = new FontAwesome.Sharp.IconMenuItem();
+            this.submenuHorarioEmpleados = new FontAwesome.Sharp.IconMenuItem();
+            this.pagosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pagosToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.deduccionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nominasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.carnetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rolesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actividadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.btnCerrarSesionAdmin = new FontAwesome.Sharp.IconButton();
+            this.panel1.SuspendLayout();
+            this.menuPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel1.Controls.Add(this.lblUsuario);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(99, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(701, 35);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.ForeColor = System.Drawing.Color.Red;
+            this.lblUsuario.Location = new System.Drawing.Point(91, 9);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(83, 13);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Nombre Usuario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Administrador:";
+            // 
+            // menuPanel
+            // 
+            this.menuPanel.BackColor = System.Drawing.Color.LightBlue;
+            this.menuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuPanel.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.asistenciaToolStripMenuItem,
+            this.menuEmpleados,
+            this.pagosToolStripMenuItem,
+            this.nominasToolStripMenuItem,
+            this.carnetToolStripMenuItem,
+            this.rolesToolStripMenuItem,
+            this.actividadToolStripMenuItem});
+            this.menuPanel.Location = new System.Drawing.Point(0, 0);
+            this.menuPanel.Name = "menuPanel";
+            this.menuPanel.Size = new System.Drawing.Size(99, 496);
+            this.menuPanel.TabIndex = 4;
+            this.menuPanel.Text = "menuStrip1";
+            // 
+            // asistenciaToolStripMenuItem
+            // 
+            this.asistenciaToolStripMenuItem.BackColor = System.Drawing.Color.LightBlue;
+            this.asistenciaToolStripMenuItem.Name = "asistenciaToolStripMenuItem";
+            this.asistenciaToolStripMenuItem.Padding = new System.Windows.Forms.Padding(4);
+            this.asistenciaToolStripMenuItem.Size = new System.Drawing.Size(86, 27);
+            this.asistenciaToolStripMenuItem.Text = "Asistencia";
+            this.asistenciaToolStripMenuItem.Click += new System.EventHandler(this.asistenciaToolStripMenuItem_Click);
+            // 
+            // menuEmpleados
+            // 
+            this.menuEmpleados.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.submenuNuevoRegistro,
+            this.submenuHorarioEmpleados});
+            this.menuEmpleados.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.menuEmpleados.IconColor = System.Drawing.Color.Black;
+            this.menuEmpleados.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.menuEmpleados.Name = "menuEmpleados";
+            this.menuEmpleados.Size = new System.Drawing.Size(86, 20);
+            this.menuEmpleados.Text = "Empleados";
+            this.menuEmpleados.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // submenuNuevoRegistro
+            // 
+            this.submenuNuevoRegistro.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.submenuNuevoRegistro.IconColor = System.Drawing.Color.Black;
+            this.submenuNuevoRegistro.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.submenuNuevoRegistro.Name = "submenuNuevoRegistro";
+            this.submenuNuevoRegistro.Size = new System.Drawing.Size(180, 22);
+            this.submenuNuevoRegistro.Text = "Nuevo Registro";
+            this.submenuNuevoRegistro.Click += new System.EventHandler(this.submenuNuevoRegistro_Click);
+            // 
+            // submenuHorarioEmpleados
+            // 
+            this.submenuHorarioEmpleados.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.submenuHorarioEmpleados.IconColor = System.Drawing.Color.Black;
+            this.submenuHorarioEmpleados.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.submenuHorarioEmpleados.Name = "submenuHorarioEmpleados";
+            this.submenuHorarioEmpleados.Size = new System.Drawing.Size(180, 22);
+            this.submenuHorarioEmpleados.Text = "Horarios Empleados";
+            // 
+            // pagosToolStripMenuItem
+            // 
+            this.pagosToolStripMenuItem.BackColor = System.Drawing.Color.LightBlue;
+            this.pagosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pagosToolStripMenuItem1,
+            this.deduccionesToolStripMenuItem});
+            this.pagosToolStripMenuItem.Name = "pagosToolStripMenuItem";
+            this.pagosToolStripMenuItem.Padding = new System.Windows.Forms.Padding(4);
+            this.pagosToolStripMenuItem.Size = new System.Drawing.Size(86, 27);
+            this.pagosToolStripMenuItem.Text = "Pagos";
+            // 
+            // pagosToolStripMenuItem1
+            // 
+            this.pagosToolStripMenuItem1.Name = "pagosToolStripMenuItem1";
+            this.pagosToolStripMenuItem1.Size = new System.Drawing.Size(142, 22);
+            this.pagosToolStripMenuItem1.Text = "Pagos";
+            // 
+            // deduccionesToolStripMenuItem
+            // 
+            this.deduccionesToolStripMenuItem.Name = "deduccionesToolStripMenuItem";
+            this.deduccionesToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
+            this.deduccionesToolStripMenuItem.Text = "Deducciones";
+            // 
+            // nominasToolStripMenuItem
+            // 
+            this.nominasToolStripMenuItem.BackColor = System.Drawing.Color.LightBlue;
+            this.nominasToolStripMenuItem.Name = "nominasToolStripMenuItem";
+            this.nominasToolStripMenuItem.Padding = new System.Windows.Forms.Padding(4);
+            this.nominasToolStripMenuItem.Size = new System.Drawing.Size(86, 27);
+            this.nominasToolStripMenuItem.Text = "Nominas";
+            // 
+            // carnetToolStripMenuItem
+            // 
+            this.carnetToolStripMenuItem.BackColor = System.Drawing.Color.LightBlue;
+            this.carnetToolStripMenuItem.Name = "carnetToolStripMenuItem";
+            this.carnetToolStripMenuItem.Padding = new System.Windows.Forms.Padding(4);
+            this.carnetToolStripMenuItem.Size = new System.Drawing.Size(86, 27);
+            this.carnetToolStripMenuItem.Text = "Carnet";
+            // 
+            // rolesToolStripMenuItem
+            // 
+            this.rolesToolStripMenuItem.Name = "rolesToolStripMenuItem";
+            this.rolesToolStripMenuItem.Size = new System.Drawing.Size(86, 19);
+            this.rolesToolStripMenuItem.Text = "Roles";
+            // 
+            // actividadToolStripMenuItem
+            // 
+            this.actividadToolStripMenuItem.BackColor = System.Drawing.Color.LightBlue;
+            this.actividadToolStripMenuItem.Name = "actividadToolStripMenuItem";
+            this.actividadToolStripMenuItem.Padding = new System.Windows.Forms.Padding(4);
+            this.actividadToolStripMenuItem.Size = new System.Drawing.Size(86, 27);
+            this.actividadToolStripMenuItem.Text = "Actividad";
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.BackColor = System.Drawing.Color.Silver;
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenedor.Location = new System.Drawing.Point(99, 35);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(701, 461);
+            this.panelContenedor.TabIndex = 7;
+            // 
+            // btnCerrarSesionAdmin
+            // 
+            this.btnCerrarSesionAdmin.BackColor = System.Drawing.Color.Crimson;
+            this.btnCerrarSesionAdmin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrarSesionAdmin.FlatAppearance.BorderSize = 0;
+            this.btnCerrarSesionAdmin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrarSesionAdmin.ForeColor = System.Drawing.Color.White;
+            this.btnCerrarSesionAdmin.IconChar = FontAwesome.Sharp.IconChar.SignOutAlt;
+            this.btnCerrarSesionAdmin.IconColor = System.Drawing.Color.White;
+            this.btnCerrarSesionAdmin.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCerrarSesionAdmin.IconSize = 15;
+            this.btnCerrarSesionAdmin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCerrarSesionAdmin.Location = new System.Drawing.Point(12, 461);
+            this.btnCerrarSesionAdmin.Name = "btnCerrarSesionAdmin";
+            this.btnCerrarSesionAdmin.Size = new System.Drawing.Size(70, 23);
+            this.btnCerrarSesionAdmin.TabIndex = 8;
+            this.btnCerrarSesionAdmin.Text = "Cerrar Sesion";
+            this.btnCerrarSesionAdmin.UseVisualStyleBackColor = false;
+            this.btnCerrarSesionAdmin.Click += new System.EventHandler(this.btnCerrarSesionAdmin_Click);
+            // 
+            // frmPanelAdministrador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 496);
+            this.Controls.Add(this.btnCerrarSesionAdmin);
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuPanel);
+            this.MaximumSize = new System.Drawing.Size(816, 535);
+            this.MinimumSize = new System.Drawing.Size(816, 535);
+            this.Name = "frmPanelAdministrador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Panel Administrador";
+            this.Load += new System.EventHandler(this.frmPanelAdministrador_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuPanel.ResumeLayout(false);
+            this.menuPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuPanel;
+        private System.Windows.Forms.ToolStripMenuItem asistenciaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pagosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pagosToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem deduccionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nominasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem carnetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rolesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actividadToolStripMenuItem;
+        private FontAwesome.Sharp.IconMenuItem menuEmpleados;
+        private FontAwesome.Sharp.IconMenuItem submenuNuevoRegistro;
+        private FontAwesome.Sharp.IconMenuItem submenuHorarioEmpleados;
+        private System.Windows.Forms.Panel panelContenedor;
+        private FontAwesome.Sharp.IconButton btnCerrarSesionAdmin;
+    }
+}

3f0611e60a57687dba296b2ee6a38ea1fc3b388f
Rick <fam.castro99@gmail.com>
2024-03-06 17:16:03 -0600

    Version Estandar v1.0

---
+++ HorarioPlus_v1.0/HorarioPlus_v1.1/Datos/Empleados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorarioPlus_v1._1.Datos
+{
+    public class Empleados
+    {
+        public string IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+        public string Rol { get; set; }
+    }
+}
---
+++ HorarioPlus_v1.0/HorarioPlus_v1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using HorarioPlus_v1._1.Presentacion;
+using System;
+using System.Windows.Forms;
+
+namespace HorarioPlus_v1._1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+        }
+    }
+}

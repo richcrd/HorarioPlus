07f16300d7a205c543af292d4d05067e730d7171
Rick <fam.castro99@gmail.com>
2024-03-06 18:17:34 -0600

    Minor Fixes

--- HorarioPlus_v1.0/HorarioPlus_v1.1/Presentacion/frmCerrarSistema.cs
+++ HorarioPlus_v1.0/HorarioPlus_v1.1/Presentacion/frmCerrarSistema.cs
@@ -13,15 +13,10 @@ namespace HorarioPlus_v1._1.Presentacion
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            // Leer el ID del empleado desde el TextBox
             string idEmpleado = txtIDconfirmacion.Text;
-
             frmLogin Busqueda = new frmLogin(); // Creamos instancia
 
-            // Buscar el empleado correspondiente al ID
             Empleados empleado = Busqueda.BuscarEmpleado(idEmpleado); // llamado
-
-            // Mostrar un mensaje con el nombre completo del empleado y la hora actual
             if (empleado != null)
             {
                 if (empleado.Rol == "Administrador")

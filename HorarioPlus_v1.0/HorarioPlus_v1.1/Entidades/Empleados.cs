bdcee1dbdce5c1eb3e24c28d2268c80ff4812cf0
Jesse <103623437+JesseNoob@users.noreply.github.com>
2024-03-30 17:51:28 -0600

    Update Empleados.cs
    Agregue propiedades del empleado como "Marcado" y métodos para poder cambiar su valor en función de la fecha

--- HorarioPlus_v1.0/HorarioPlus_v1.1/Entidades/Empleados.cs
+++ HorarioPlus_v1.0/HorarioPlus_v1.1/Entidades/Empleados.cs
@@ -21,26 +21,54 @@ namespace HorarioPlus_v1._1.Datos
         public double TotalHorasAcumuladas { get; set; }
         public double SalarioNeto { get; set; }
         public string HorarioEmpleado { get; set; }
-        public List<RegistroAsistencia> Registros { get; set; } = new List<RegistroAsistencia>();
-
-        public Empleados() // Constructor vacio debido a los archivos json
-        {
-
-        }
-
-        public Empleados(string id, string nombre, string apellido1, string apellido2, int edad, string correo, string rol, double pagoPorHoras, double totalHorasAcumuladas, double salarioNeto, string horarioEmpleado)
-        {
-            this.IdEmpleado = id;
-            this.Nombre = nombre;
-            this.Apellido1 = apellido1;
-            this.Apellido2 = apellido2;
-            this.Edad = edad;
-            this.Correo = correo;
-            this.Rol = rol;
-            this.PagoPorHoras = pagoPorHoras;
-            this.TotalHorasAcumuladas = totalHorasAcumuladas;
-            this.SalarioNeto = salarioNeto;
-            this.HorarioEmpleado = horarioEmpleado;
-        }
+         //Datos cronometricos del empleado
+         public List<GestionDelTiempo> RegistroDelTiempo { get; set; } = new List<GestionDelTiempo>();
+         public bool Marcado { get; set; } = false;
+         public Empleados() {}
+        
+         public Empleados(string id, string nombre, string apellido1, string apellido2, int edad, string correo, string rol, double pagoPorHoras, double totalHorasAcumuladas, double salarioNeto, string horarioEmpleado)
+         {
+             IdEmpleado = id;
+             Nombre = nombre;
+             Apellido1 = apellido1;
+             Apellido2 = apellido2;
+             Edad = edad;
+             Correo = correo;
+             Rol = rol;
+             PagoPorHoras = pagoPorHoras;
+             TotalHorasAcumuladas = totalHorasAcumuladas;
+             SalarioNeto = salarioNeto;
+             HorarioEmpleado = horarioEmpleado;
+         }
+         
+         #region Metodos_Adicionales
+         public static bool CambiarValorLogico()
+         {
+             //cambiar a false la propiedad "Marcado" si el dia es diferente del actual
+             List<Empleados> ListaEmpleados = ManejadorEmpleados.lista_Empleados;
+        
+             for (int i = 0; i < ListaEmpleados.Count(); i++)
+             {
+                 int ultimoElemento = ListaEmpleados[i].RegistroDelTiempo.Count() - 1;
+                 if (ListaEmpleados[i].RegistroDelTiempo[ultimoElemento].FechaMarcada != GestionDelTiempo.HOY)
+                 {
+                     ListaEmpleados[i].Marcado = false;
+                     ManejadorEmpleados.ActualizarEmpleado(ManejadorEmpleados.lista_Empleados, ListaEmpleados[i].IdEmpleado, ListaEmpleados[i]);
+                 }
+             }
+             return false;
+         }
+         public static void BorrarRegistro()
+         {
+             List<Empleados> ListaEmpleados = ManejadorEmpleados.lista_Empleados;
+             for (int i = 0; i < ListaEmpleados.Count(); i++)
+             {
+                 ListaEmpleados[i].RegistroDelTiempo.Clear();
+                 ListaEmpleados[i].Marcado = false;
+                 ManejadorEmpleados.ActualizarEmpleado(ManejadorEmpleados.lista_Empleados, ListaEmpleados[i].IdEmpleado, ListaEmpleados[i]);
+        
+             }
+         }
+         #endregion
     }
 }
